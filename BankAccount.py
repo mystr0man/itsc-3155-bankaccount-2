b7cd9907ded854ebbdead42df0f0440a24514098
Magnetize8953 <aha6@charlotte.edu>
2025-02-12 16:04:55 -0500

    feat: add bool returns to BankAccount methods

--- BankAccount.py
+++ BankAccount.py
@@ -9,17 +9,21 @@ class BankAccount:
         self._routing_number = routing_number
 
 
-    def deposit(self, dep):
+    def deposit(self, dep) -> bool:
         if dep >= 0:
             self.current_balance += dep
+            return True
+        return False
 
-    def withdraw(self, wd):
+    def withdraw(self, wd) -> bool:
             if self.current_balance - wd < self.minimum_balance:
                 print("Error withdrawing $" + str(wd) + " from an account with $" + str(self.current_balance) + ": remaining balance would be below minimum balance!")
+                return False
             else:
                 self.current_balance -= wd
+                return True
 
-    def print_customer_information(self):
+    def print_customer_information(self) -> None:
         print("At " + self.bank_title + ", " + self.customer_name + " has a balance of " + str(self.current_balance) +
               ", and their account has a minimum balance of " + str(self.minimum_balance));
 

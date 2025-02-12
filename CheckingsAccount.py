5197513061d2690c16be814c083648e743249931
Magnetize8953 <aha6@charlotte.edu>
2025-02-12 16:07:55 -0500

    feat: impl CheckingsAccount transfer method

--- CheckingsAccount.py
+++ CheckingsAccount.py
@@ -5,5 +5,10 @@ class CheckingAccount(BankAccount):
         super().__init__(customer_name, current_balance, minimum_balance, account_number, routing_number)
         self.transfer_limit = transfer_limit
 
-    def transfer(self):
-        pass
+    def transfer(self, other: BankAccount, amount: float) -> bool:
+        if self.withdraw(amount):
+            other.deposit(amount)
+            return True
+        else:
+            print('Insufficient funds to transfer')
+            return False

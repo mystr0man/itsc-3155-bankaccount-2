c10ba62b0a4989504314dc7adc317a0660612371
Magnetize8953 <aha6@charlotte.edu>
2025-02-12 15:36:32 -0500

    feat: add CheckingAccount class

---
+++ CheckingsAccount.py
@@ -0,0 +1,9 @@
+from BankAccount import BankAccount
+
+class CheckingAccount(BankAccount):
+    def __init__(self, customer_name, current_balance, minimum_balance, transfer_limit):
+        super().__init__(customer_name, current_balance, minimum_balance)
+        self.transfer_limit = transfer_limit
+
+    def transfer(self):
+        pass

2975c43f0d2fc64507bbda1bb6c5dcfec3535d67
Devdan Ferguson <dfergu25@charlotte.edu>
2025-02-12 15:49:51 -0500

    feat: added private/protected members

--- BankAccount.py
+++ BankAccount.py
@@ -1,10 +1,13 @@
 class BankAccount:
     bank_title = "Truist"
 
-    def __init__(self, customer_name, current_balance, minimum_balance):
+    def __init__(self, customer_name, current_balance, minimum_balance, account_number, routing_number):
         self.customer_name = customer_name
         self.current_balance = current_balance
         self.minimum_balance = minimum_balance
+        self.__account_number = account_number
+        self._routing_number = routing_numer
+
 
     def deposit(self, dep):
         if dep >= 0:
